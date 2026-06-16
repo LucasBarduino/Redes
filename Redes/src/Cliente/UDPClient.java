3a4f1537e68fb77ab7fc481557d9f425511949ee
LucasBarduino <91089040+LucasBarduino@users.noreply.github.com>
2026-06-16 15:42:27 -0300

    Add files via upload

---
+++ Redes/src/Cliente/UDPClient.java
@@ -0,0 +1,42 @@
+package cliente;
+
+import java.net.*;
+
+public class UDPClient {
+
+    public static void verificarServidor(String ip)
+            throws Exception {
+
+        DatagramSocket socket =
+                new DatagramSocket();
+
+        String mensagem = "PING";
+
+        DatagramPacket pacote =
+                new DatagramPacket(
+                        mensagem.getBytes(),
+                        mensagem.length(),
+                        InetAddress.getByName(ip),
+                        6000);
+
+        socket.send(pacote);
+
+        byte[] buffer = new byte[1024];
+
+        DatagramPacket resposta =
+                new DatagramPacket(
+                        buffer,
+                        buffer.length);
+
+        socket.receive(resposta);
+
+        System.out.println(
+                "Resposta UDP: " +
+                        new String(
+                                resposta.getData(),
+                                0,
+                                resposta.getLength()));
+
+        socket.close();
+    }
+}
